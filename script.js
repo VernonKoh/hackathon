83644da8f829dcb1752ff870472f01020b95d860
VernonKoh <s10248408@connect.np.edu.sg>
2024-03-10 15:49:57 +0800

    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let header = document.querySelector("header");
+  
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+  
+let menu = document.querySelector(".navbar");
+  
+document.querySelector("#menu-icon").onclick = () => {
+  menu.classList.toggle("active");
+};
+
+/* remove shadow on scroll */
+window.onscroll = () => {
+  menu.classList.remove("active");
+};
